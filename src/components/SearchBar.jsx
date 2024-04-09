a57ff21edcfe4b6e034f122aec1b3c180e6ff1db
Carlos Gámez <hardpx@gmail.com>
2024-04-08 23:04:01 -0600

    searc bar update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,26 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+import { useState } from 'react';
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState('');
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    const result = setTerm(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onSubmit(term);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input onChange={handleChange} type='text' value={term} />
+      <button type='submit'>Search</button>
+    </form>
+  );
 }
 
 export default SearchBar;
